629e7b741e59a0ec7a2d3d80afc8891a1acc6c78
Lorenzo Alves <lorenzo@quant1.com.br>
2024-01-20 04:33:50 -0300

    feat(frontend): add/update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Client from './components/Client';
+
+function App() {
+  return (
+    <div className="App">
+      <Client />
+    </div>
+  );
+}
+
+export default App;
